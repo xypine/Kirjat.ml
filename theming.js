198631e88fdea25c29e04cdec53e9712d45e915a
Elias Eskelinen <elias.eskelinen@pm.me>
2021-01-18 20:46:29 +0200

    hmm

--- theming.js
+++ theming.js
@@ -3,7 +3,25 @@ var textC = "text-dark";
 var bgC = "bg-white";
 var bg2C = "bg-light";
 var btnC = "";
-if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && false){
+function applyTheme(){
+	
+}
+function toggleDark(theme){
+	if(theme === "light"){
+		darktheme = false;
+		textC = "text-dark";
+		bgC = "bg-white";
+		bg2C = "bg-light";
+		btnC = "";
+	}
+	else if(theme === "dark"){
+		darktheme = true;
+		textC = "text-light";
+		bgC = "bg-black";
+		bg2C = "bg-dark";
+	}
+}
+if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1 || true){
      console.log("platform seems to be firefox, allowing theming...")
      console.log("theming init.");
      const themeConfig = new ThemeConfig();
@@ -13,6 +31,8 @@ if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && false){
      textC = "text-light";
      bgC = "bg-black";
      bg2C = "bg-dark";
+     const darkSwitch = writeDarkSwitch(themeConfig);
+     themeConfig.themeChangeHandlers.push(theme => toggleDark(theme));
 }
 else{
 	console.log("Platform seems to not be firefox, theming won't probably work.");
