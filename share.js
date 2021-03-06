f14a399c0b9467e88551288289b1c5b896a5b5cf
Elias Eskelinen <elias.eskelinen@protonmail.com>
2021-03-06 23:45:40 +0200

    disabled the sharing button if sharing is not available

--- share.js
+++ share.js
@@ -1,3 +1,8 @@
+if (!navigator.share){
+	console.log("Sharing not available! Deleting the share button...");
+	document.documentElement.style.setProperty('--copybtn-width', "98.5%");
+	document.querySelector("#share").style.display = "none";
+}
 function share(kurl){
 	if (navigator.share) {
 		navigator.share({
